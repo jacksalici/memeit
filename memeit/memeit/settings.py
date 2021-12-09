2ad6e8bcaa4ce75494cb70cee65553d2710d6f94
Jack <giambo2000@gmail.com>
2021-12-09 18:54:18 +0100

    secrets are secrets

--- memeit/memeit/settings.py
+++ memeit/memeit/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-obdkgme50i6oa4ey#i5@6b-so(k_%)&$4ii%7v6pxh+ar!qpv2'
+import decouple as config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
